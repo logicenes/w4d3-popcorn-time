898dcb250b88fe8d1a11028c5ee5acc8c703d0a0
„Enes <enes.gnay@yahoo.de>
2025-07-09 12:54:07 +0200

    deleteing movies

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,25 +1,39 @@
+import { useState } from "react";
+
 import movies from "../data/movies.json"
 
 function MovieList() {
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies)
+    const deleteMovie = (movieId) => {
+        const newList = moviesToDisplay.filter((movie) => {
 
+            return movie.id !== movieId;
+               
+        })
+        setMoviesToDisplay(newList)
+    }
     return (
-            <>
+        <>
 
-            <h1>Movielist...</h1>
+            <h1>Number of Movies: {moviesToDisplay.length}</h1>
 
-            {movies.map((movieObj, i, arr) => {
-               return ( 
-               <div key={movieObj.id}>
-                 <h3>{movieObj.title}</h3> 
-                 <img src={movieObj.imgURL} alt="" />"
-                 <p>Year: {movieObj.year}</p>
-                 <p>Rating: {movieObj.rating}</p>
-                 </div>
-               )
+            {moviesToDisplay.map((movieObj, i, arr) => {
+                return (
+                    <div key={movieObj.id} className="card">
+                        <h3>{movieObj.title}</h3>
+                        <img src={movieObj.imgURL} alt="" />
+                        <p>Year: {movieObj.year}</p>
+                        <p>Rating: {movieObj.rating}</p>
+
+                        <p>
+                            <button className="delte-btn" onClick={() => { deleteMovie(movieObj.id) }}>Delete this movie</button>
+                        </p>
+                    </div>
+                )
             })}
-            </>
+        </>
     )
-}  
-            
+}
+
 
 export default MovieList;
